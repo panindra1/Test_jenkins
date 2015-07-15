56216c7ba41994a334a01ad6e5a91802529dd9fa
panindra Tumkur Seetharamu <ptumkurseetharamu@godaddy.com>
2015-07-15 12:58:01 -0700

    CHecking jenkins build when pushed

--- GuildBrowser/WoWApiClient.m
+++ GuildBrowser/WoWApiClient.m
@@ -25,7 +25,8 @@ static dispatch_once_t oncePredicate;
     if (!self) {
         return nil;
     }
-    
+    NSLog(@"Nothing");
+
     [self registerHTTPOperationClass:[AFJSONRequestOperation class]];
     
     // Accept HTTP Header; see http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.1
